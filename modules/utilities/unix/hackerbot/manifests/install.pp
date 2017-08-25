967d7810f2d0fa4be80ae0dfa15c9bf67afaa0dd
Z. Cliffe Schreuders <code.z.cliffe@schreuders.org>
2017-08-25 15:45:47 +0100

    WiP

--- modules/utilities/unix/hackerbot/manifests/install.pp
+++ modules/utilities/unix/hackerbot/manifests/install.pp
@@ -1,13 +1,17 @@
 class hackerbot::install{
+  package { ['sshpass', 'ruby', 'rubygems']:
+    ensure => 'installed',
+  }
+
   package { ['cinch', 'nokogiri', 'nori', 'programr']:
     ensure => 'installed',
     provider => 'gem',
   }
 
   file { 'hackerbot files':
-    path => '/opt/hackerbot',
+    path => '/opt/hackerbot/',
     ensure => recursive,
-    source => 'puppet:///modules/hackerbot',
+    source => 'puppet:///modules/hackerbot/opt_hackerbot/',
   }
 
 }
