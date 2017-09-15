5387794b1b7b33f70721dee1dd88e1e7f7993041
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2017-09-15 14:36:24 +0100

    WiP

--- modules/utilities/unix/hackerbot/manifests/install.pp
+++ modules/utilities/unix/hackerbot/manifests/install.pp
@@ -15,25 +15,19 @@ class hackerbot::install{
     group => 'root',
   }->
 
-    file { "/opt/hackerbot/hackerbot.rb":
-      ensure  => file,
-      content  => template('hackerbot/hackerbot.rb.erb'),
-      require => File['/opt/hackerbot'],
-    }
+  file { "/opt/hackerbot/hackerbot.rb":
+    ensure  => file,
+    content  => template('hackerbot/hackerbot.rb.erb'),
+    require => File['/opt/hackerbot'],
+  }
 
   $secgen_parameters['hackerbot_configs'].each |$counter, $config_pair| {
     $parsed_pair = parsejson($config_pair)
-    # $hackerbot_xml_configs = $hackerbot_xml_configs + $parsed_pair['xml_config']
-    # $hackerbot_lab_sheets = $hackerbot_lab_sheets + $parsed_pair['html_lab_sheet']
-    # notice($hackerbot_xml_configs)
-    # alert($hackerbot_lab_sheets)
 
-    # alert($parsed_pair['xml_config'])
-    # $num = $counter + 1
     alert("creating bot config")
-    $filename = "bot_$counter.xml"
+    $xmlfilename = "bot_$counter.xml"
 
-    file { "/opt/hackerbot/config/$filename":
+    file { "/opt/hackerbot/config/$xmlfilename":
       ensure => present,
       content => $parsed_pair['xml_config'],
       mode   => '0600',
@@ -45,54 +39,27 @@ class hackerbot::install{
       ensure => 'directory',
     }
 
-    $filename = "lab_r$counter.html"
+    $htmlfilename = "lab_r$counter.html"
 
-    file { "/var/www/$filename":
+    file { "/var/www/$htmlfilename":
       ensure => present,
       content => $parsed_pair['html_lab_sheet'],
     }
 
   }
 
-
-
-  # # FIXME: not needed if the above happens first, but there seems to be some ordering problems
-  # exec { "create-config-dir":
-  #   path    => ['/bin', '/usr/bin', '/usr/local/bin', '/sbin', '/usr/sbin'],
-  #   command => "mkdir -p /opt/hackerbot/config",
-  #   provider => shell,
-  # }
-
-  # $hackerbot_xml_configs.each |$counter, $config| {
-  #   alert($counter)
-  #   $num = $counter + 1
-  #   $filename = "bot_$num.xml"
-  #
-  #   file { "/opt/hackerbot/config/$filename":
-  #     ensure => present,
-  #     content => $config,
-  #     mode   => '0600',
-  #     owner => 'root',
-  #     group => 'root',
-  #   }
-  # }
-  #
-  # $hackerbot_lab_sheets.each |$counter, $sheet| {
-  #   $num = $counter + 1
-  #   $filename = "lab_x_$num.html"
-  #
-  #   file { "/var/www/$filename":
-  #     ensure => present,
-  #     content => $sheet,
-  #   }
-  # }
+  package { ['nori', 'cinch', 'programr']:
+    ensure   => 'installed',
+    provider => 'gem',
+  } ->
 
   file { '/etc/systemd/system/hackerbot.service':
     ensure => 'link',
     target => '/opt/hackerbot/hackerbot.service',
   }~>
+  # reload services (networking needs to be reloaded on the kali virtualbox vm)
   exec { 'hackerbot-systemd-reload':
-    command     => 'systemctl daemon-reload',
+    command     => 'systemctl daemon-reload; service networking restart; service hackerbot restart',
     path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
     refreshonly => true,
   }
