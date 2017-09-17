00c18e6c7b2626d51709502a44eea4da6b6dc6bd
Z. Cliffe Schreuders <code.cliffe@schreuders.org>
2017-09-17 15:48:50 +0100

    about to do something drastic...

---
+++ modules/utilities/unix/hackerbot/manifests/config.pp
@@ -0,0 +1,50 @@
+class hackerbot::config{
+  require hackerbot::install
+
+  $secgen_parameters = secgen_functions::get_parameters($::base64_inputs_file)
+  $server_ip = $secgen_parameters['server_ip'][0]
+  $port = $secgen_parameters['port'][0]
+
+  $hackerbot_xml_configs = []
+  $hackerbot_lab_sheets = []
+
+  $secgen_parameters['hackerbot_configs'].each |$counter, $config_pair| {
+    $parsed_pair = parsejson($config_pair)
+
+    alert("creating bot config")
+    $xmlfilename = "bot_$counter.xml"
+
+    file { "/opt/hackerbot/config/$xmlfilename":
+      ensure => present,
+      content => $parsed_pair['xml_config'],
+      mode   => '0600',
+      owner => 'root',
+      group => 'root',
+    }
+
+    $htmlfilename = "lab_r$counter.html"
+
+    file { "/var/www/labs/$htmlfilename":
+      ensure => present,
+      content => $parsed_pair['html_lab_sheet'],
+    }
+
+  }
+
+  file { "/opt/hackerbot/hackerbot.rb":
+    ensure  => file,
+    content  => template('hackerbot/hackerbot.rb.erb'),
+    require => File['/opt/hackerbot'],
+  }
+
+  class { '::apache':
+    default_vhost => false,
+    # overwrite_ports => false,
+  }
+  apache::vhost { 'vhost.labs.com':
+    port    => $port,
+    docroot => '/var/www/labs',
+  }
+
+
+}
